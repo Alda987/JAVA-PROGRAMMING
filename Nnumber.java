9a4e9a86bdbea9d817cf0a3ad3e209261a229997
Alda987 <aldatreesajosy05@gmail.com>
2025-07-16 10:33:30 +0530

    Add files via upload

---
+++ Nnumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Nnumber {
+	
+	
+	 public static void main(String [] args) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("first+n+number are:");
+		 int n=sc.nextInt();
+		 for(int i=0;i<n;i++){
+			 System.out.println(i +"");
+		 }
+}
+}
