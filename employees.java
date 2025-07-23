2e323431188be2385b258a564c0b48bca7eca53b
Alda987 <aldatreesajosy05@gmail.com>
2025-07-23 10:52:14 +0530

    Add files via upload

---
+++ employees.java
@@ -0,0 +1,22 @@
+package input;
+
+class Employee{
+	String name;
+	int id;
+	double salary;
+  void displayEmployee() {
+	 
+	 System.out.println("EmployeeName:" + name);
+	 System.out.println("age:" + age);
+}
+	
+}
+public class student {
+	public static void main(String[] args) {
+		Student1 student=new Student1();
+		student.displaystudentinfo("alda",20);
+	}
+}
+public class employees {
+
+}
