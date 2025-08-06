828bd17c3d9d0c2e57181b3d9d5b68068253921d
Alda987 <aldatreesajosy05@gmail.com>
2025-08-06 15:26:51 +0530

    Add files via upload

--- book.java
+++ book.java
@@ -4,7 +4,7 @@
 public class book {
     String title;
     String author;
-    final int bookid =0;
+    final int bookId;
     static  int bookCounter=1000;
     static final String LIBRARY_NAME = "cENTRAL LIBRARY";
 
@@ -12,29 +12,61 @@ public class book {
     book() {
         this.title = title;
         this.author=author;
+        this.bookId=bookCounter++;
         }
-    book(String title,String author) {
+    book(String title,String author,int bookId) {
         this.title = title;
         this.author=author;
+        this.bookId=bookCounter++;
         }
         
+    public void displayinfo() {
+    	System.out.println("TITLE"+title);
+    	System.out.println("AUTHOR"+author);
+    	System.out.println("ID"+bookId);
+    	
         
-        
-        studentCount++;
+      
     }
-
-    public static int getStudentCount() {
-        return studentCount;
+    
+    public void displayinfo(boolean showLibrary) {
+    	if (showLibrary==true) {
+    		System.out.println("LIBRARY_NAME"+LIBRARY_NAME);
+    	}
+    	else {
+    		System.out.println(" it is false");
+    	}
     }
-}
+    
+    public static void displaybooks() {
+    	System.out.println("total books added"+(bookCounter -1000))  ;
+    }
+    
 
 
-class Main {
+
     public static void main(String[] args) {
-        Student s1 = new Student("ALICE");
-        Student s2 = new Student("ALONA");
-        Student s3 = new Student("ELNAQ");
+        book b1 = new book();
+        book b2 = new book("J.K ROWLING","HARRY POTTER",1001);
+        book b3 = new book("AUTHOR CLONING","SHERLOCK HOMES",1003);
+        book b4 = new book("R.K NARAYANAN","MALGAUNDI DAYS",1004);
 
-        System.out.println("TOTAL NO OF STUDENTS: " + Student.getStudentCount());
+        System.out.println("book info:");
+        b1.displayinfo();
+        
+        System.out.println("book info:");
+        b2.displayinfo(true);
+        b2.displayinfo();
+        
+        System.out.println("book info:");
+        b3.displayinfo(true);
+        b3.displayinfo();
+        
+        System.out.println("book info:");
+        b4.displayinfo(true);
+        b4.displayinfo();
+        System.out.println("book info:");
+        book.displaybooks();
+        
     }
 }
